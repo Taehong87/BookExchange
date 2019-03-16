fc19a67b095f09b6e2922b1ef28453cafe7413ea
Tomred <Tomred87@hotmail.com>
2019-03-15 19:11:07 -0700

    Update MainMenu.php

--- MainMenu.php
+++ MainMenu.php
@@ -2,5 +2,20 @@
 	session_start();
 	$name_email = 'name=' . $_SESSION['name'] .
 				  '&email=' . $_SESSION['email'];
-	print $name_email;
+	#print $name_email;
 ?>
+
+<ul>
+<li>
+	<a href="shopping.php?<?php print $name_email; ?>" /> shopping
+	</a>
+</li>
+<li>
+	<a href="selling.php?<?php print $name_email; ?>" /> selling
+	</a>
+</li>
+<li>
+	<a href="profile.php?<?php print $name_email; ?>" /> profile
+	</a>
+</li>
+</ul>
