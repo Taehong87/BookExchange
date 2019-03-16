8145f213cb06478fbb36d4bf7ef18b3cf1e764ef
Tomred <Tomred87@hotmail.com>
2019-03-15 18:29:27 -0700

    Update login.php

--- login.php
+++ login.php
@@ -25,12 +25,13 @@
 
 	if (mysqli_num_rows($results) > 0) 
 	{
-		
-		#header("Location: main.php");
-		#exit;
+		session_start();
+		$_SESSION['name'] = $_POST["name"];
+		$_SESSION['email'] = $_POST["email"];
+		header("Location: main.php");
+		exit;
 	}
 
 ?>
-</table>
 </body>
 </html>
