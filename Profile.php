49669ba347cbe6c310dea04102f96b3f5b0751c9
FeedMeSleep <32755702+FeedMeSleep@users.noreply.github.com>
2019-05-05 22:35:33 -0700

    Add files via upload

---
+++ Profile.php
@@ -0,0 +1,51 @@
+<!doctype html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <title>Profile Page</title>
+    </head>
+
+    <body>
+        <?php
+            include("MainMenu.php");
+        ?>
+
+        <table align = "center">
+
+            <tr>
+            
+                <td>Name:</td>
+                <td></td>
+
+            </tr>
+
+            <tr>
+
+                <td>Email:</td>
+                <td></td>
+
+            </tr>
+
+            <tr>
+
+                <td>CIN:</td>
+                <td></td>
+
+            </tr>
+
+            <tr>
+
+                <td>Profile Picture:</td>
+                <td></td>
+
+            </tr>
+
+        </table>        
+    
+        <a href = EditProfile.php align = "center"> Edit Profile <a>
+
+    </body>
+
+</html>
