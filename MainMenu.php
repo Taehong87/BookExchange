d5332f4675f16ecce3b81f3ee6e7aebcf41a2afe
TheManWithAPlan <amzn.only14@gmail.com>
2019-05-06 14:51:21 -0700

    Adding id to Session data
    Adding id to Session data

--- MainMenu.php
+++ MainMenu.php
@@ -3,22 +3,24 @@
 
 <?php
 	session_start();
-	$name_email = 'name=' . $_SESSION['name'] .
-				  '&email=' . $_SESSION['email'];
-	#print $name_email;
+	$name_email_id = 'name=' . $_SESSION['name'] .
+				  '&email=' . $_SESSION['email'] .
+				  '&id=' . $_SESSION['id'];
+	//print $name_email_id;
+	
 ?>
 
 <ul class="ul_css">
 <li class="li_css">
-	<a class="li_css_a" href="shopping.php?<?php print $name_email; ?>" /> shopping
+	<a class="li_css_a" href="shopping.php?<?php print $name_email_id; ?>" /> shopping
 	</a>
 </li>
 <li class="li_css">
-	<a class="li_css_a" href="selling.php?<?php print $name_email; ?>" /> selling
+	<a class="li_css_a" href="selling.php?<?php print $name_email_id; ?>" /> selling
 	</a>
 </li>
 <li class="li_css">
-	<a class="li_css_a" href="profile.php?<?php print $name_email; ?>" /> profile
+	<a class="li_css_a" href="profile.php?<?php print $name_email_id; ?>" /> profile
 	</a>
 </li>
 </ul>
